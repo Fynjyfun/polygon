b48334fea358ce44e6553b943f4f89d08359cd5a
Anton <fynjyfun@gmail.com>
2026-05-22 23:53:30 +0300

    Editor impl

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './styles/main.css';
+import './components/AppCanvas';
+import './components/ToolBar';
+import './components/InfoPanel';
+import { Scene } from './models/Scene';
+import { CommandManager } from './models/CommandManager';
+
+const scene = new Scene();
+const commands = new CommandManager();
+
+const canvas = document.querySelector('app-canvas');
+const toolbar = document.querySelector('tool-bar');
+const info = document.querySelector('info-panel');
+
+canvas.setScene(scene);
+canvas.setCommandManager(commands);
+toolbar.init(scene, commands, canvas);
+info.setScene(scene);
